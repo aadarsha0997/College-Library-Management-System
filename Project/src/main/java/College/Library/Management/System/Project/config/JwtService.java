c505a05f4d3dd677eff66e8cd0215bcb13ea26bd
Aadarsha <aadarsha0997@gmail.com>
2026-08-09 20:12:38 +0545

    flet: GenerateToken function added

--- Project/src/main/java/College/Library/Management/System/Project/config/JwtService.java
+++ Project/src/main/java/College/Library/Management/System/Project/config/JwtService.java
@@ -1,15 +1,21 @@
 package College.Library.Management.System.Project.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
+
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
+
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
-import java.util.Base64;
 
 @Service
 public class JwtService {
@@ -30,6 +36,20 @@ public class JwtService {
         final Claims claims=extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+
+    public String generateToken(
+            Map<String,Object> extraClaims,
+            UserDetails userDetails){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignKey())
+                .compact();
+    }
+
 
     private Claims extractAllClaims(String token){
         return Jwts.parser()
