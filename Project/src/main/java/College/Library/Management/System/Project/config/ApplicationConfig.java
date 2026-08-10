6168c506b91716e5ed70c763da79a3c9450fa7b4
Aadarsha <aadarsha0997@gmail.com>
2026-08-10 17:03:19 +0545

    Congigute JWT authentication and Spring Security

--- Project/src/main/java/College/Library/Management/System/Project/config/ApplicationConfig.java
+++ Project/src/main/java/College/Library/Management/System/Project/config/ApplicationConfig.java
@@ -4,9 +4,15 @@ import College.Library.Management.System.Project.Repo.StudentRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,4 +28,25 @@ public class ApplicationConfig {
             }
         }
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+
+        DaoAuthenticationProvider authProvider=
+                new DaoAuthenticationProvider( userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
